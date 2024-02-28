4cd0c93b100b7ce9771a711c079fd59ea37e5a87
blanklavender <rudrapatimahima248@gmail.com>
2024-02-29 05:24:42 +0530

    api working with psql

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,24 @@
-from flask import Flask, request, jsonify
-import cbf_pipeline.cbf as cbf
-import collab_algo as col
-from pymongo import MongoClient
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_cors import CORS
-from auth import auth as auth_blueprint
-from db import entries
-from main import main as main_blueprint
-from cbf_pipeline.scrap import check_path
-import os
-import csv
+import psycopg2 
+from config import load_config
+import cbf as cbf
+import collab_algo as col
+# from auth import auth as auth_blueprint
+# from main import main as main_blueprint
+# from pymongo import MongoClient
+# import os
+# import csv
+# from db import entries
+# from cbf_pipeline.scrap import check_path
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://localhost:27017')
+# client = MongoClient('mongodb://localhost:27017')
+# app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
-
-app.register_blueprint(auth_blueprint)
-app.register_blueprint(main_blueprint)
+# app.register_blueprint(auth_blueprint)
+# app.register_blueprint(main_blueprint)
 
 CORS(app) 
 
@@ -25,12 +26,12 @@ CORS(app)
 def hello_world():
     return "Hello, World!"
 
-@app.route('/receive-location', methods=['POST'])
-def receive_location():
-    location = request.json.get('location')
-    print(location)
-    result = check_path(location)
-    return jsonify(result)
+# @app.route('/receive-location', methods=['POST'])
+# def receive_location():
+#     location = request.json.get('location')
+#     print(location)
+#     result = check_path(location)
+#     return jsonify(result)
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
@@ -68,34 +69,60 @@ def get_restaurant_details():
 
     restaurant_details = []
     for restaurant_name in restaurant_names:
-        details = fetch_details_from_csv(location, restaurant_name)
+        details = fetch_details(location, restaurant_name)
         if details:
             restaurant_details.append(details)
 
     return jsonify(restaurant_details[:7])
 
-def fetch_details_from_csv(location, restaurant_name):
-    file_path = "../backend/dataset/" + location
+def fetch_details(location, restaurant_name):
+    # file_path = "../backend/dataset/" + location
 
+    config = load_config()
     try:
-        with open(os.path.join(file_path, f"{location}_profile.csv"), newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                if row.get("restaurant_name") == restaurant_name:
-                    return {
-                        "restaurant_name": row.get("restaurant_name", "N/A"),
-                        "budget": row.get("budget", "N/A"),
-                        "type": row.get("restaurant_type", "N/A"),
-                        "rating": row.get("avg_rating", "N/A"),
-                        "img_file_path": file_path + "/reviews/" + restaurant_name + '/' + restaurant_name+ ".jpg",
-                        "rec_dishes": row.get("rec_dishes", "N/A"),
-                        "rating_graph": file_path + "/graphs/" + restaurant_name + '/' + "rating_graph.jpg",
-                        "sentiment_graph": file_path + "/graphs/" + restaurant_name + '/' + "sentiment_graph.jpg",
-                    }
-        return None  # Restaurant not found
-    except Exception as e:
-        print(f"Error fetching details from CSV: {e}")
+        with psycopg2.connect(**config) as conn:
+            cur = conn.cursor()
+            get_query = f"""
+                SELECT rest_name, rest_budget, rest_cuisine, rest_rating, rest_img, rev_count
+                FROM test.restaurants
+                WHERE rest_name = '{restaurant_name}'
+            """
+            cur.execute(get_query)
+            row = cur.fetchone()
+
+            return {
+                "restaurant_name": f"{row[0]}",
+                "budget": f"{row[1]}",
+                "type": f"{row[2]}",
+                "rating": f"{row[3]}",
+                "img_file_path": f"{row[4]}",
+                "rev_count":f"{row[5]}"
+            }
+
+
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
         return None
+
+    # try:
+    #     with open(os.path.join(file_path, f"{location}_profile.csv"), newline='') as csvfile:
+    #         reader = csv.DictReader(csvfile)
+    #         for row in reader:
+    #             if row.get("restaurant_name") == restaurant_name:
+    #                 return {
+    #                     "restaurant_name": row.get("restaurant_name", "N/A"),
+    #                     "budget": row.get("budget", "N/A"),
+    #                     "type": row.get("restaurant_type", "N/A"),
+    #                     "rating": row.get("avg_rating", "N/A"),
+    #                     "img_file_path": file_path + "/reviews/" + restaurant_name + '/' + restaurant_name+ ".jpg",
+    #                     "rec_dishes": row.get("rec_dishes", "N/A"),
+    #                     "rating_graph": file_path + "/graphs/" + restaurant_name + '/' + "rating_graph.jpg",
+    #                     "sentiment_graph": file_path + "/graphs/" + restaurant_name + '/' + "sentiment_graph.jpg",
+    #                 }
+    #     return None  # Restaurant not found
+    # except Exception as e:
+    #     print(f"Error fetching details from CSV: {e}")
+    #     return None
 
 if __name__ == '__main__':
     app.run(debug=True)
