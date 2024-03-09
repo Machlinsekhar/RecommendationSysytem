7750a91cb3ca0e5b231f2179e171c202d840f680
blanklavender <rudrapatimahima248@gmail.com>
2024-03-09 18:51:04 +0530

    user_ratings table updating

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, session
+from flask import Blueprint, request, session, jsonify
 import json
 from middleware import needs_auth
 from config import load_config
@@ -47,6 +47,66 @@ def set_entry(account):
         except Exception as e:
             conn.rollback()
             raise Exception("An error occurred: {}".format(e))
+        
 
+@main.route('/fetch-top-from-location', methods=['POST'])
+@needs_auth()
+def fetch_rest(account):
+    print(account)
+    print(session)
+    user_id = session.get('uid')
+    print("from session: ", user_id)
 
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
+    print(request.form)
+
+    if request.method == 'POST':
+        try:
+            cursor.execute(f"""
+                SELECT home_location FROM test.users WHERE user_id={user_id};
+            """)
+            location = cursor.fetchone()[0]
+
+            cursor.execute(f"""
+                SELECT rest_id, rest_name FROM test.restaurants WHERE rest_location='{location}';
+            """)
+            rest_from_location = cursor.fetchall()
+
+            return jsonify(rest_from_location)
+        
+        except Exception as e:
+            conn.rollback()
+            raise Exception("An error occurred: {}".format(e))
+
+@main.route('/store-user-ratings', methods=['POST'])
+@needs_auth()
+def set_rating_entry(account):
+    user_id = session.get('uid')
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    if request.method == 'POST':
+        ratings = request.get_json()
+        print(ratings)
+
+        try:
+            cursor.execute(f"""
+                SELECT home_location FROM test.users WHERE user_id={user_id};
+            """)
+            location = cursor.fetchone()[0]
+
+            for id, rating in ratings.items():
+                cursor.execute(f"""
+                    INSERT INTO test.user_rating(user_id, rest_id, user_rating, loc_name)
+                    VALUES (%s,%s,%s,%s)
+                """, (user_id, id, rating, location))
+
+            conn.commit()
+            return {"message": "Done"}, 200
+
+        except Exception as e:
+            conn.rollback()
+            raise Exception("An error occurred: {}".format(e))
+        
+        finally:
+            cursor.close()
